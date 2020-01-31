d4adc20de245c9e2fa224c75c78ea99c92706cf7
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-01-31 15:16:41 +0100

    feat: implemented install-aliases.js

--- install-aliases.js
+++ install-aliases.js
@@ -1,1 +1,39 @@
-console.log("installing");
+const fs = require("fs");
+const path = require("path");
+const shell = require("shelljs");
+
+const dir = process.env.INIT_CWD;
+
+if (!dir) {
+  throw new Error(
+    "INIT_CWD not available: if using Yarn, ensure it is on version > 1.19"
+  );
+}
+
+const package = require(`${dir}/package.json`);
+const targetDir = path.resolve(dir, package.gitAliasesDir || `./git-aliases`);
+
+if (!fs.existsSync(targetDir)) {
+  console.info(
+    `git-local-aliases is installed, but no aliases were found at ${targetDir}`
+  );
+  process.exit();
+}
+
+const gitRoot = shell
+  .exec("git rev-parse --show-toplevel", { cwd: dir })
+  // remove line breaks
+  .stdout.replace(/\r?\n|\r/g, "")
+  // remove weird `/private` prefix during tests
+  .replace(/^\/private/, "");
+
+const aliases = fs.readdirSync(targetDir);
+
+for (const alias of aliases) {
+  const pathToAliasFile = path.relative(
+    gitRoot,
+    path.resolve(targetDir, alias)
+  );
+
+  shell.exec(`git config alias.${alias} '!${pathToAliasFile}'`, { cwd: dir });
+}
