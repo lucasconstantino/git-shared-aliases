5bd09b73769f7edf4f5a37abf58f1de2eb380de9
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-01-31 18:06:05 +0100

    test: added integration tests (yarn/npm)

--- install-aliases.test.js
+++ install-aliases.test.js
@@ -41,4 +41,47 @@ describe("install-aliases", () => {
     expect(result.code).toBe(0);
     expect(result.stdout).toContain("node-alias ok");
   });
+
+  describe("integration", () => {
+    it("[Yarn] should install git aliases", () => {
+      result = exec(
+        `cd ${dir}; INIT_CWD=${dir} yarn add --dev ${__dirname}`,
+        options
+      );
+
+      expect(result.code).toBe(0);
+
+      // check config setup
+      const config = readFileSync(`${dir}/.git/config`).toString();
+      expect(config).toContain("[alias]");
+      expect(config).toContain("bash-alias = !git-aliases/bash-alias");
+      expect(config).toContain("node-alias = !git-aliases/node-alias");
+
+      result = exec(`git bash-alias`, { ...options, cwd: dir });
+      expect(result.code).toBe(0);
+      expect(result.stdout).toContain("bash-alias ok");
+
+      result = exec(`git node-alias`, { ...options, cwd: dir });
+      expect(result.code).toBe(0);
+      expect(result.stdout).toContain("node-alias ok");
+    });
+
+    it("[npm] should install git aliases", () => {
+      result = exec(`cd ${dir}; npm install --dev ${__dirname}`, options);
+
+      // check config setup
+      const config = readFileSync(`${dir}/.git/config`).toString();
+      expect(config).toContain("[alias]");
+      expect(config).toContain("bash-alias = !git-aliases/bash-alias");
+      expect(config).toContain("node-alias = !git-aliases/node-alias");
+
+      result = exec(`git bash-alias`, { ...options, cwd: dir });
+      expect(result.code).toBe(0);
+      expect(result.stdout).toContain("bash-alias ok");
+
+      result = exec(`git node-alias`, { ...options, cwd: dir });
+      expect(result.code).toBe(0);
+      expect(result.stdout).toContain("node-alias ok");
+    });
+  });
 });
