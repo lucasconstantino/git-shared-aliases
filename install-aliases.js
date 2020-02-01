2b19d50f2fcdba5ec864c7bba542c19d6f918843
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-02-01 21:21:21 +0100

    feat: allow recursive alias install

--- install-aliases.js
+++ install-aliases.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const shell = require("shelljs");
 const pkgDir = require("pkg-dir");
+const glob = require("glob");
 
 if (!process.env.INIT_CWD) {
   throw new Error(
@@ -9,31 +10,59 @@ if (!process.env.INIT_CWD) {
   );
 }
 
-const dir = pkgDir.sync(process.env.INIT_CWD).replace(/^\/private/, "");
-const package = require(`${dir}/package.json`);
-const targetDir = path.resolve(dir, package.gitAliasesDir || `./git-aliases`);
+const rootDir = pkgDir.sync(process.env.INIT_CWD).replace(/^\/private/, "");
+const globOptions = { realpath: true, cwd: rootDir };
 
-if (!fs.existsSync(targetDir)) {
-  console.info(
-    `git-shared-aliases is installed, but no aliases were found at ${targetDir}`
-  );
+const aliases = []
+  .concat(
+    glob.sync("**/node_modules/*/git-aliases/*", globOptions),
+    glob.sync("./git-aliases/*", globOptions)
+  )
+  .map(cleanPath)
+  .map(toAlias)
+  .filter(uniqueAlias);
+
+if (!aliases.length) {
+  console.info(`git-shared-aliases is installed, but no aliases were found.`);
   process.exit();
 }
 
-const gitRoot = shell
-  .exec("git rev-parse --show-toplevel", { cwd: dir })
+const gitRoot = cleanPath(
   // remove line breaks
-  .stdout.replace(/\r?\n|\r/g, "")
-  // remove weird `/private` prefix during tests
-  .replace(/^\/private/, "");
-
-const aliases = fs.readdirSync(targetDir);
+  shell.exec("git rev-parse --show-toplevel", { cwd: rootDir }).stdout
+);
 
 for (const alias of aliases) {
-  const pathToAliasFile = path.relative(
-    gitRoot,
-    path.resolve(targetDir, alias)
-  );
+  const pathToAliasFile = path.relative(gitRoot, alias.path);
 
-  shell.exec(`git config alias.${alias} '!${pathToAliasFile}'`, { cwd: dir });
+  shell.exec(`git config alias.${alias.name} '!${pathToAliasFile}'`, {
+    cwd: rootDir
+  });
+}
+
+function cleanPath(pathStr) {
+  return (
+    pathStr
+      // replace any line-breaks
+      .replace(/\r?\n|\r/g, "")
+      // remove weird `/private` prefix during tests
+      .replace(/^\/private/, "")
+  );
+}
+
+function toAlias(fullPath) {
+  return {
+    path: fullPath,
+    name: path.basename(fullPath)
+  };
+}
+
+function uniqueAlias(alias, i, aliases) {
+  for (const j in aliases) {
+    if (j > i && aliases[j].name === alias.name) {
+      return false;
+    }
+  }
+
+  return true;
 }
--- install-aliases.test.js
+++ install-aliases.test.js
@@ -1,10 +1,12 @@
-const { readFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
 const { dirSync, setGracefulCleanup } = require("tmp");
 const { exec } = require("shelljs");
+const directPackage = require("./examples/direct/package.json");
 
 setGracefulCleanup();
 
 const options = { cwd: __dirname, fatal: true, silent: true };
+const directPackageStr = JSON.stringify(directPackage, null, 2);
 
 describe("install-aliases", () => {
   describe("direct", () => {
@@ -76,6 +78,91 @@ describe("install-aliases", () => {
         expect(config).toContain("[alias]");
         expect(config).toContain("bash-alias = !git-aliases/bash-alias");
         expect(config).toContain("node-alias = !git-aliases/node-alias");
+
+        result = exec(`git bash-alias`, { ...options, cwd: dir });
+        expect(result.code).toBe(0);
+        expect(result.stdout).toContain("bash-alias ok");
+
+        result = exec(`git node-alias`, { ...options, cwd: dir });
+        expect(result.code).toBe(0);
+        expect(result.stdout).toContain("node-alias ok");
+      });
+    });
+  });
+
+  describe("indirect", () => {
+    let result;
+    let tmp;
+    let dir;
+
+    beforeEach(() => {
+      result = undefined;
+      tmp = dirSync();
+
+      direct = `${tmp.name}/direct/`;
+      dir = `${tmp.name}/indirect/`;
+
+      // cleanup
+      exec(`rm -Rf ./examples/direct/node_modules`, options);
+      exec(`rm -Rf ./examples/indirect/node_modules`, options);
+
+      exec(`cp -Rf ./examples/direct/ ${direct}`, options);
+      exec(`cp -Rf ./examples/indirect/ ${dir}`, options);
+
+      writeFileSync(
+        direct + "package.json",
+        directPackageStr.replace("../", __dirname)
+      );
+
+      exec(`cd ${dir}; git init`, options);
+    });
+
+    // safe use of afterEach because of graceful cleanup set above.
+    afterEach(() => tmp.removeCallback());
+
+    describe("integration", () => {
+      it("[Yarn] should install git aliases", () => {
+        result = exec(
+          `cd ${dir}; INIT_CWD=${dir} yarn add --dev ${direct}`,
+          options
+        );
+
+        expect(result.code).toBe(0);
+
+        // check config setup
+        const config = readFileSync(`${dir}/.git/config`).toString();
+        expect(config).toContain("[alias]");
+        expect(config).toContain(
+          "bash-alias = !node_modules/git-shared-aliases--direct/git-aliases/bash-alias"
+        );
+        expect(config).toContain(
+          "node-alias = !node_modules/git-shared-aliases--direct/git-aliases/node-alias"
+        );
+
+        result = exec(`git bash-alias`, { ...options, cwd: dir });
+        expect(result.code).toBe(0);
+        expect(result.stdout).toContain("bash-alias ok");
+
+        result = exec(`git node-alias`, { ...options, cwd: dir });
+        expect(result.code).toBe(0);
+        expect(result.stdout).toContain("node-alias ok");
+      });
+
+      it.skip("[npm] should install git aliases", () => {
+        console.log({ dir });
+        result = exec(`cd ${dir}; npm install --dev ${direct}`, options);
+
+        console.log({ result });
+
+        // check config setup
+        const config = readFileSync(`${dir}/.git/config`).toString();
+        expect(config).toContain("[alias]");
+        expect(config).toContain(
+          "bash-alias = !node_modules/git-shared-aliases--direct/git-aliases/bash-alias"
+        );
+        expect(config).toContain(
+          "node-alias = !node_modules/git-shared-aliases--direct/git-aliases/node-alias"
+        );
 
         result = exec(`git bash-alias`, { ...options, cwd: dir });
         expect(result.code).toBe(0);
